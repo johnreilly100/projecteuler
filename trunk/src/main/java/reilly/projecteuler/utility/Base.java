e5aad60d5ac249bc61aff17ef57c828a7f65a982
johnreilly <johnreilly@5c6d3d8b-5686-4ac7-b1c5-51cf15174b98>
2012-10-18 08:04:23 +0000

    update to the Base class

--- trunk/src/main/java/reilly/projecteuler/utility/Base.java
+++ trunk/src/main/java/reilly/projecteuler/utility/Base.java
@@ -1,12 +1,10 @@
 package reilly.projecteuler.utility;
 
 public class Base {
-    public static void main(String[] args) 
-    {
-        System.out.println("Start");
+    
+    public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-//        System.out.println("xxxxxxx = " + xxxxxxxxxx +"\t yyyyyyyyyyyyyy = " + yyyyyyyyyyyyy);
         System.out.println("Time taken = " + ((System.currentTimeMillis() - startTime) / 1000));
     }
 }
