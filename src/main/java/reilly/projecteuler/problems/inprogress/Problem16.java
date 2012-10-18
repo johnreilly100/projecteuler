8a1b30e0dcb92a4338c13d04b750339de491d9c2
johnreilly <johnreilly@5c6d3d8b-5686-4ac7-b1c5-51cf15174b98>
2012-10-18 20:07:22 +0000

    problem 19 complete

--- src/main/java/reilly/projecteuler/problems/inprogress/Problem16.java
+++ src/main/java/reilly/projecteuler/problems/inprogress/Problem16.java
@@ -1,7 +1,5 @@
 package reilly.projecteuler.problems.inprogress;
 
-import java.math.BigDecimal;
-
 import static java.lang.Math.pow;
 
 /**
@@ -18,13 +16,18 @@ public class Problem16 {
 
         //problem is to convert 2^1000 from binary into decimal
         // binary answer is 1 with 1000 zeros after it.
-        double temp = pow(2, 700);
-//        BigInteger result = BigInteger.valueOf(pow(2, 1000));
-        BigDecimal bd = new BigDecimal(pow(2, 500));
-        bd.
-        Integer.parseInt("10000", 2);
+        double temp = pow(2, 500);
+        String number = Double.toString(temp);
+//        Double.toArray();
+//        final int[] intArray = new int[temp.length];
+//        for (int i=0; i<intArray.length; ++i)
+//            intArray[i] = (int) doubleArray[i];
+////        BigInteger result = BigInteger.valueOf(pow(2, 1000));
+////        BigInteger reallyBig = new BigInteger();
+//        BigDecimal bd = new BigDecimal(pow(2, 1000));
+//        Integer.parseInt("10000", 2);
 
-        System.out.println("result = " + bd);
+        System.out.println("result = " + number);
         System.out.println("Time taken = " + ((System.currentTimeMillis() - startTime) / 1000));
     }
 }
