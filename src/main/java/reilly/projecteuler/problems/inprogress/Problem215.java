44dd2abe85599be8ef90d69153c16a621d3420cd
johnreilly <johnreilly@5c6d3d8b-5686-4ac7-b1c5-51cf15174b98>
2012-10-31 17:08:21 +0000

    mid way through it

--- src/main/java/reilly/projecteuler/problems/inprogress/Problem215.java
+++ src/main/java/reilly/projecteuler/problems/inprogress/Problem215.java
@@ -13,6 +13,28 @@ public class Problem215 {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         Problem215 problem215 = new Problem215();
+        int[] testRow = new int[9];
+        System.out.println("testRow.length = " + testRow.length);
+        for(int i = 0; i < testRow.length; i++) {
+            testRow[i] = -1;
+        }
+//        problem215.printArray(testRow);
+//        problem215.addThreeToRow(testRow, 9);
+//        problem215.printArray(testRow);
+//        problem215.addThreeToRow(testRow, 9);
+//        problem215.printArray(testRow);
+//        problem215.addThreeToRow(testRow, 9);
+//        problem215.printArray(testRow);
+//        problem215.addThreeToRow(testRow, 9);
+//        problem215.printArray(testRow);
+//        problem215.addThreeToRow(testRow, 9);
+//        problem215.printArray(testRow);
+        ArrayList<int []> rowConfigurations = new ArrayList<int[]>();
+        problem215.calculateAllLayoutsForOneRow(testRow, 9, rowConfigurations);
+        System.out.println("rowConfigurations size = " + rowConfigurations.size());
+        for(int[] row: rowConfigurations) {
+            problem215.printArray(row);
+        }
 
         int[] originRow = new int[33];
         for(int i = 0; i < 33; i++) {
@@ -28,18 +50,93 @@ public class Problem215 {
         System.out.println("Time taken = " + ((System.currentTimeMillis() - startTime) / 1000));
     }
 
-    ArrayList<int []> calculateAllBlockLayoutsForOneRow(ArrayList<int []> rowConfiguration, int rowSize) {
-
-
-
-        for(int[] row: rowConfiguration) {
-            if(row.length == rowSize)
-            // add a block of two
-
-            // add a block of three
+    void printArray(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            if(i == array.length -1) {
+                System.out.println(array[i]);
+            } else {
+                System.out.print(array[i]);
+            }
         }
-        List<Integer> rowLayout = new ArrayList<Integer>();
+    }
+    
+    int[] copyArray(int[] source) {
+        int[] destination = new int[source.length];
+        System.arraycopy(source, 0, destination,0 , source.length);
+        return destination;
+    }
 
-        return rowConfigurations;
+//    ArrayList<int []> calculateAllBlockLayoutsForOneRow(ArrayList<int []> rowConfiguration, int rowSize) {
+//
+//
+//
+//        for(int[] row: rowConfiguration) {
+//            if(row.length == rowSize)
+//            // add a block of two
+//
+//            // add a block of three
+//        }
+//        List<Integer> rowLayout = new ArrayList<Integer>();
+//
+//        return rowConfigurations;
+//    }
+//
+    void calculateAllLayoutsForOneRow(int[] row, int rowLength, ArrayList<int []> rowConfigurations) {
+        int[] firstCopy = copyArray(row);
+        if(addTwoToRow(firstCopy, rowLength)) {
+            if(checkIfRowIsFull(firstCopy, rowLength)) {
+                rowConfigurations.add(firstCopy);    
+            } else {
+                calculateAllLayoutsForOneRow(firstCopy, rowLength, rowConfigurations);
+            }
+        }
+        int[] secondCopy = copyArray(row);
+        if(addThreeToRow(secondCopy, rowLength)) {
+            if(checkIfRowIsFull(secondCopy, rowLength)) {
+                rowConfigurations.add(secondCopy);
+            } else {
+                calculateAllLayoutsForOneRow(secondCopy, rowLength, rowConfigurations);
+            }
+        }
+    }
+
+    boolean checkIfRowIsFull(int[] row, int rowLength) {
+        if(row[rowLength - 1] != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean addTwoToRow(int[] row, int rowLength) {
+        int nextFreeIndex = calculateNexFreeIndex(row);
+        if(rowLength - nextFreeIndex < 2) {
+            return false;
+        } else {
+            row[nextFreeIndex] = 1;
+            row[nextFreeIndex + 1] = 0;
+           return true;
+        }
+    }
+
+    boolean addThreeToRow(int[] row, int rowLength) {
+        int nextFreeIndex = calculateNexFreeIndex(row);
+        if(rowLength - nextFreeIndex < 3) {
+            return false;
+        } else {
+            row[nextFreeIndex] = 1;
+            row[nextFreeIndex + 1] = 0;
+            row[nextFreeIndex + 2] = 0;
+            return true;
+        }
+    }
+    
+    int calculateNexFreeIndex(int[] row) {
+        for(int i = 0; i < row.length; i++) {
+            if(row[i] == -1) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
